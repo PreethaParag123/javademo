058a3e5b088840bc3ef7e7f1c3e5993e8f9c341a
PreethaParag123 <106303081+PreethaParag123@users.noreply.github.com>
2022-10-13 18:07:35 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableSwagger2
+/*kjdnka*/
 public class DemoApplication {
 
 	public static void main(String[] args) {
